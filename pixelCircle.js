4448a3e444529b39b79096813e5298806f4bdf30
SnowWalker030 <kuekzhengyu@gmail.com>
2022-11-13 00:26:00 +0800

    sync js

--- pixelCircle.js
+++ pixelCircle.js
@@ -2,29 +2,23 @@ const inp = document.querySelector('#inpR');
 const ls = document.querySelector('#ls');
 const mat = document.querySelector('#matrix');
 const pix = document.querySelector('#pixel');
-let r, intR, r_2, limit, Rx, dx;
+let r, r_2, intR, limit, Rx, dx;
 let Arr1 = [];
 let Arr2 = [];
 
 function draw(){
-    intR = r%1;
     r_2 = r**2;
+    intR = r%1;
     limit = Math.floor(r / 2**.5 - intR);
-    Rx = Math.floor(r) * (Math.ceil(r)-1);
+    Rx = r_2 - r + .25;
     dx = r*2 - 2;
     Arr1 = [];
     Arr2 = [];
 
-    for(i=r-0.5;i>limit;i--){
-        if(intR){
-            Arr1.push(
-                Math.floor((r_2-Rx)**.5) + 0.5
-            );
-        }else{
-            Arr1.push(
-                Math.round((r_2-Rx-0.25)**.5)
-            );
-        };
+    for(i=r-.5;i>limit;i--){
+        Arr1.push(
+            Math.round((r_2-Rx)**.5 + intR) - intR
+        );
         Rx -= dx;
         dx -= 2;
     };
@@ -37,7 +31,7 @@ function draw(){
 };
 function checkC(){
     r = Number(inp.value);
-    if(r>=0.5 && r%0.5==0){
+    if(r>=.5 && r%.5==0){
         if(r <= 10000000){
             draw();
         }else if(r > 10000000 && confirm(`你输入的半径( ${r})已超过 1千万，\n继续操作可能造成设备停顿。\n是否继续？`)){
@@ -48,7 +42,7 @@ function checkC(){
     };
 };
 function checkL(){
-    if(r>=0.5 && r%0.5==0){
+    if(r>=.5 && r%.5==0){
         if(r <= 100000){
             renderL();
         }else if(r > 100000 && confirm(`你输入的半径( ${r})已超过 10万，\n继续操作可能造成设备停顿。\n是否继续？（ 建议尝试生成Array ）`)){
@@ -59,7 +53,7 @@ function checkL(){
     };
 };
 function checkM(){
-    if(r>=0.5 && r%0.5==0){
+    if(r>=.5 && r%.5==0){
         if(r <= 5000000){
             renderM();
         }else if(r > 5000000 && confirm(`你输入的半径( ${r})已超过 500万，\n继续操作可能造成设备停顿。\n是否继续？（ 建议Ctrl+Shift+J直接查看Console面板 ）`)){
@@ -70,7 +64,7 @@ function checkM(){
     };
 };
 function checkP(){
-    if(r>=0.5 && r%0.5==0){
+    if(r>=.5 && r%.5==0){
         if(r <= 5000){
             renderP();
         }else if(r > 5000 && confirm(`你输入的半径( ${r})已超过 5000，\n继续操作可能造成设备停顿。\n是否继续？（ 建议尝试使用Excel生成器 ）`)){
@@ -83,7 +77,7 @@ function checkP(){
 function renderL(){
     ls.style.display = '';
     mat.style.display = 'none';
-    pix.style = 'none';
+    pix.style.display = 'none';
     ls.innerHTML = "<tr><th id='no'>No</th><th id='y'>Radius_Y</th></tr>"
     for(i=0;i<Arr2.length;i++){
         li = document.createElement('tr');
@@ -94,18 +88,17 @@ function renderL(){
 function renderM(){
     ls.style.display = 'none';
     mat.style.display = '';
-    pix.style = 'none';
+    pix.style.display = 'none';
     max = Arr2[0].toString().length;
     regSpace = RegExp(`^.*(.{${max}})$`,'g');
     link = JSON.stringify(Arr2).replace(/\[|\]/g,'');
-    console.log(link)
     link = link.replace(/,\d+/g,addSpace);
     mat.innerHTML = link;
 };
 function renderP(){
     ls.style.display = 'none';
     mat.style.display = 'none';
-    pix.style = '';
+    pix.style.display = '';
     map = '';
     a = Math.floor(Arr1[Arr1.length-1]) - limit;
     b = Arr1[Arr1.length-1] + Arr1.length - a;
